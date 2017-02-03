fdec6dfe3edc042968af725513219dae087f90c0
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-03 14:14:44 -0800

    Setup express server

---
+++ bin/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const http = require('http');
+const config = require('../config');
+const app = require('../src/server/webapp');
+const sockets = require('../src/server/sockets');
+const log = require('../src/util/log');
+
+let server = http.createServer(app);
+let io = require('socket.io')(server);
+sockets.init(io);
+
+server.listen(config.port, () => {
+  log(`Express server listening on port *:${config.port}`);
+});
