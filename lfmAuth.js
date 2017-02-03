9547d5a31da50650ad4b4a112a2d46a70d4d46a7
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-03 15:41:19 -0800

    Generate a md5 hashed signature

--- lfmAuth.js
+++ lfmAuth.js
@@ -2,21 +2,36 @@
 
 const request = require('./src/util/request');
 const config = require('./config');
+const crypto = require('crypto');
 
-// let callbackUrl = "http://localhost:3000"
-// let apiAuthUrl = "http://www.last.fm/api/auth/?api_key=" + config.apiKey + "&cb=" + callbackUrl;
 let apiRootUrl = "http://ws.audioscrobbler.com/2.0/";
 let getTokenUrl = apiRootUrl + "?method=auth.gettoken&api_key=" + config.apiKey + "&format=json"
-let authToken = '';
+let rawSignature = '';
+let auth = {};
 
+// request unauthorized request token for API account
 request.get(getTokenUrl)
   .then((data) => {
     console.log(data);
+    // auth tokens are valid for 60 mins
     return JSON.parse(data).token;
   })
   .then((token) => {
-    console.log(token);
+    // generate signature
+    rawSignature = "api_key" + config.apiKey + "methodauth.getSession" + "token" + token + config.apiSecret;
+    console.log(rawSignature);
+    return rawSignature;
+  })
+  .then((signature) => {
+    //hash the signature using md5
+    auth.signedSignature = crypto
+      .createHash('md5')
+      .update(signature, 'utf8')
+      .digest('hex')
+    console.log(auth);    
   })
   .catch((err) => {
     console.log(err);
   })
+
+  module.exports = auth;
