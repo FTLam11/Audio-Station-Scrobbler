00eefa80f4fec2d35f0ec66aa0b5c4eca7e80fff
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-04 11:48:30 -0800

    Obtain session key

--- lfmAuth.js
+++ lfmAuth.js
@@ -5,8 +5,8 @@ const config = require('./config');
 const crypto = require('crypto');
 
 let apiRootUrl = "http://ws.audioscrobbler.com/2.0/";
-let getTokenUrl = apiRootUrl + "?method=auth.gettoken&api_key=" + config.apiKey + "&format=json"
-let getSessionUrl = apiRootUrl + "?method=auth.getSession"
+let getTokenUrl = apiRootUrl + "?method=auth.gettoken&api_key=" + config.apiKey + "&format=json";
+let getSessionUrl = apiRootUrl + "?method=auth.getSession";
 let rawSignature = '';
 let auth = {};
 
@@ -46,5 +46,14 @@ request.get(getTokenUrl)
   .catch((err) => {
     console.log(err);
   })
+
+request.get(getSessionUrl + "&token=fe114c7ef0dffb638be8a448b84d1d23" + 
+  "&api_key=" + config.apiKey + "&api_sig=a01a71384e24a6062f5f9ea926074de6" + "&format=json")
+  .then((session) => {
+    console.log(session);
+    auth.sessionKey = session;
+    console.log("Auth is ", auth);
+  })
+  .catch((err) => console.log(err))
 
   module.exports = auth;
