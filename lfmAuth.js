54c5b7e27a9d9543d27c39c1561a6b3faee980d7
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-03 20:33:32 -0800

    Request session key

--- lfmAuth.js
+++ lfmAuth.js
@@ -6,20 +6,23 @@ const crypto = require('crypto');
 
 let apiRootUrl = "http://ws.audioscrobbler.com/2.0/";
 let getTokenUrl = apiRootUrl + "?method=auth.gettoken&api_key=" + config.apiKey + "&format=json"
+let getSessionUrl = apiRootUrl + "?method=auth.getSession"
 let rawSignature = '';
 let auth = {};
 
 // request unauthorized request token for API account
 request.get(getTokenUrl)
   .then((data) => {
-    console.log(data);
+    // console.log(data);
     // auth tokens are valid for 60 mins
-    return JSON.parse(data).token;
+    // user needs to authorized application access for token to be valid
+    auth.token = JSON.parse(data).token;
+    return auth.token;
   })
   .then((token) => {
     // generate signature
     rawSignature = "api_key" + config.apiKey + "methodauth.getSession" + "token" + token + config.apiSecret;
-    console.log(rawSignature);
+    // console.log(rawSignature);
     return rawSignature;
   })
   .then((signature) => {
@@ -28,7 +31,17 @@ request.get(getTokenUrl)
       .createHash('md5')
       .update(signature, 'utf8')
       .digest('hex')
-    console.log(auth);    
+    // console.log(auth);
+    return auth.signedSignature;    
+  })
+  .then((hash) => {
+    request.get(getSessionUrl + "&token=" + auth.token + "&api_key=" + config.apiKey + "&api_sig=" + hash)
+      .then((session) => {
+        console.log(session);
+        auth.sessionKey = session;
+        console.log("Auth is ", auth);
+      })
+      .catch((err) => console.log(err))
   })
   .catch((err) => {
     console.log(err);
