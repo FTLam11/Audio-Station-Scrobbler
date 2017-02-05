42884d325d6cde6835dac4213c7260da68d971f9
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-05 02:13:12 -0800

    Rename scrobble to track router

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const router = require('express').Router();
-const authRouter = require('./auth/authRouter');
+const trackRouter = require('./track/trackRouter');
 
 router.get('/', function(req, res) {
   res.send('Scrawble dat track jo!');
 });
 
-router.use('/auth', authRouter);
+router.use('/tracks', trackRouter);
 
 module.exports = router;
