c77f36aa39fa95208dd25f9db39abee804aa2c90
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-05 02:12:31 -0800

    Add session key to config and update api key

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@
 let config = {};
 
 config.port = 3000;
-config.apiKey = process.env.LFM_API_KEY;
+config.api_key = process.env.LFM_API_KEY;
 config.apiSecret = process.env.LFM_SECRET;
+config.sk = process.env.LFM_SESSION_KEY;
 
 module.exports = config;
