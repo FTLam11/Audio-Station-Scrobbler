4afdd07198b54fc229799ba40146703c25933e7e
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-06 13:24:02 -0800

    Parse web session request URL

--- src/server/middleware/parse.js
+++ src/server/middleware/parse.js
@@ -4,17 +4,18 @@ let validParams = function(queryObj) {
   return queryObj.api_key != null &&
              queryObj.token != null &&
              queryObj.secret != null;
-}
+};
 
 module.exports = function() {
   return (req, res, next) => {
     if (validParams(req.query)) {
       req.body.api_key = req.query.api_key;
+      req.body.method = 'auth.getSession';
       req.body.token = req.query.token;
       req.body.secret = req.query.secret;
       next();
     } else {
-      next(new Error("Invalid parameters for session request."));
-    }
+      next(new Error("Missing parameters for session request."));
+    };
   };
 };
