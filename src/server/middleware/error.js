c3786c79eabeab111072f4a580a0847ddebbff77
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-06 12:51:08 -0800

    Update loggin in error handler

--- src/server/middleware/error.js
+++ src/server/middleware/error.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const log = require('../../util/log');
+
 module.exports = () => {
   return function(err, req, res, next) {
     if (err) {
-      console.log(err.stack);
+      log.error(err.stack);
       res.status(500).send(err);
     }
   };
