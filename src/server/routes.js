72e510af97576471fe9552e420063f5acc23cc07
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-06 12:50:29 -0800

    Mount auth routes

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const router = require('express').Router();
+const authRouter = require('./auth/authRouter');
 const trackRouter = require('./track/trackRouter');
 
 router.get('/', function(req, res) {
   res.send('Scrawble dat track jo!');
 });
+
+router.use('/auth', authRouter);
 
 router.use('/tracks', trackRouter);
 
