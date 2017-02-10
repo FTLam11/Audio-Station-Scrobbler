82781d533c8948901538ca7a904a696eda4cf441
frosted_fronks <ryzingsun11@yahoo.com>
2017-02-10 15:59:33 -0800

    Mount middleware and assign controller action

--- src/server/track/trackController.js
+++ src/server/track/trackController.js
@@ -1,32 +1,22 @@
 'use strict';
 
 const request = require('../../util/request');
+const querystring = require('querystring');
 
 let controller = {};
-let dataString = "hello";
-let options = {
-  hostname: 'ws.audioscrobbler.com',
-  path: '/2.0/',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(dataString)
-  }
-};
 
 controller.create = (req, res, next) => {
-  request.post(options, dataString);
-  console.log(request);
-  // res.json("AWwWWWWWW Shiiiiiiiiiiiiieeeeeeeeeeeet");
+  request.post(req.body.options, req.body.dataString)
+    .then((lastfmres) => {
+      res.json(lastfmres);
+    })
+    .catch((err) => {
+      next(err);
+    });
   // create a scrobbling queue, since NAS submits 
   // lyric requests for the currently playing song
   // and the next song in playlist
   // Last.fm uses 4 minutes as the time interval for scrobbles
-}
-
-controller.test = (req, res, next) => {
-  console.log("SUP DAWG");
-  res.json("We fucking did it");
-}
+};
 
 module.exports = controller;
--- src/server/track/trackRouter.js
+++ src/server/track/trackRouter.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const trackRouter = express.Router();
 const controller = require('./trackController');
+const env = require('../middleware/loadEnv');
 const payload = require('../middleware/payload');
 const squish = require('../middleware/payload');
 const sign = require('../middleware/signature');
+const stringify = require('../middleware/stringify');
+const options = require('../middleware/options');
 
 trackRouter.route(/\/q/)
-  // .post(payload.build(), controller.create)
-  .post(controller.test)
-  .get(controller.test)
+  .get(payload.build(), payload.squish(), sign(), stringify(), options(), controller.create)
 
 module.exports = trackRouter; 
