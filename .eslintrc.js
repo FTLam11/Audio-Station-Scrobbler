ba329d866b165edfb2bad3d9c0a0128f15f1c8a2
Frank Lam <ryzingsun11@yahoo.com>
2020-07-13 16:13:55 +0800

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2020: true,
+    mocha: true,
   },
   extends: [
     'airbnb-base',
@@ -11,5 +12,9 @@ module.exports = {
     ecmaVersion: 11,
   },
   rules: {
+    'no-unused-vars': ['error', { args: 'none' }],
+    'func-names': ['error', 'never'],
+    'no-console': ['error', { allow: ['log', 'debug', 'error'] }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
 };
