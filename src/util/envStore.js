77b6ae8ca9072083fc2888ba66341177325924b2
Frank Lam <ryzingsun11@yahoo.com>
2020-07-13 15:50:15 +0800

    Extract variable in envStore

--- src/util/envStore.js
+++ src/util/envStore.js
@@ -5,8 +5,9 @@ const fs = require('fs');
 // Save API info to be loaded as environment variables later
 module.exports = function(key, value) {
   return () => {
-    process.env[`${key.toUpperCase()}`] = value;
-    fs.appendFileSync('./env.js',
-      `process.env['${key.toUpperCase()}'] = '${value}'\n`)
+    const envar = key.toUpperCase();
+
+    process.env[envar] = value;
+    fs.appendFileSync('./env.js', `process.env['${envar}'] = '${value}'\n`)
   };
 };
